f07266d3115f8545911b21ed24b5f2c8c5043fbd
Aminul <a.soton7@gmail.com>
2026-02-15 23:38:52 +0600

    feat: update database configuration to use DATABASE_URL for Sequelize connection

--- src/config/db.js
+++ src/config/db.js
@@ -1,16 +1,15 @@
 const { Sequelize } = require("sequelize");
 require("dotenv").config();
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASS,
-  {
-    host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT,
-    logging: false,
-    port: process.env.DB_PORT,
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: "postgres",
+  logging: false,
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000,
   },
-);
+});
 
 module.exports = sequelize;
